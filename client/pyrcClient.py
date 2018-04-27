f21c569d258d93da42372fa4dc16ebfd9cfa13f5
Levi Schubert <me@levischubert.com>
2018-04-27 15:22:57 -0500

    chat function working, connection currently hardcoded to pyrc.tk

--- client/pyrcClient.py
+++ client/pyrcClient.py
@@ -1,4 +1,4 @@
-# PyRC Client [Version 0.0.1]
+# PyRC Client [Version 0.1.0]
 # Author: Levi Schubert [me@Levischubert.com]
 # Description: A python client to send and receive chat messages with its corresponding server
 # Date: 4/25/18
@@ -30,7 +30,7 @@ def connect():
     global host
     global sock
     sock = socket.socket()
-    host = socket.gethostname()
+    host = "pyrc.tk" #socket.gethostname()
     connected = True
 
 def setName():
@@ -38,18 +38,30 @@ def setName():
     name = input("Please choose a username to be displayed on servers: ")
     print("Your username has been set to: " + name)
 
+def printChat(sock, dont):
+	while True:
+		data = sock.recv(1024).decode()
+		if not data:
+			break
+		print(data)
+
+
 def chat():
     global connected
     global name
     global chatting
     global sock
     chatting = True
+    counter = 0
     if connected == False:
         print("Error: you are not connected to a server")
     elif(name == ""):
         print("Error: you have not set a username")
     else:
         print("System: You are now chatting with the server. Use /exit to leave the chat.")
+        sock.connect((host,7972))
+        _thread.start_new_thread(printChat, (sock, None))
+        sock.send(name.encode())
         while(chatting):
             arg = input()
             if(arg.split(' ',1)[0] == "/exit"):
@@ -57,10 +69,10 @@ def chat():
                 print("You're now disconnected from the chat")
             else:
                 print("\033[A                                                                                \033[A")
-                sock.connect((host,7000))
-                sock.send(arg)
-                print(sock.recv(1024))
-                sock.close()
+                sock.send(arg.encode())
+                # sock.send(("message number: " + str(counter)).encode() )
+                # counter += 1
+    sock.close()
             #parse input to send chat
 
 def interpret(com):
@@ -79,6 +91,12 @@ def interpret(com):
 
 def main():
     global command
+    print("Welcome to PyRC [Version 0.1.0]")
+    print("Type 'help' to get started")
+    print("User Test version usage:")
+    print("type 'setName' and follow the prompt to set a username")
+    print("type connect | The ip and port are already set for the test")
+    print("type chat to enter the chat room and connect")
     command = input()
     while(command != "exit"):
         interpret(command)
--- server/pyrcServer.py
+++ server/pyrcServer.py
@@ -1,4 +1,4 @@
-# PyRC Server [Version 0.0.2]
+# PyRC Server [Version 0.1.0]
 # Author: Levi Schubert [me@levischubert.com]
 # Description: a Python Relay Chat server lightweight enough to run on a Raspberry Pi
 #              with AES data encryption
@@ -9,37 +9,82 @@ import _thread
 #import cryptography
 import socket
 
+#global variables
+global messages
+global index
+
+messages = []
+index = 0
+
+#remove the previous thousand messages from the buffer and save to a log file
+def clearOld():
+	global messages
+	with open("log.txt", "a") as log:
+		for msg in messages:
+			log.write(msg)
+	
+
+def addMessage(msg):
+	global messages
+	global index
+	if(index > 1000):
+		clearOld()
+		messages = []
+		index = -1
+	messages.append(msg)
+	index += 1
+	# print("added '" + msg + "' to global array")
+
+def sendChat(conn, localIndex):
+	while True:
+		if(localIndex != index):
+			conn.send(messages[localIndex].encode())
+			localIndex += 1
+			if(localIndex >= 1000):
+				localIndex = 0
+
 def handling(conn, addr):
-    user = conn.recv(1024).decode()
-    while True:
-        data = conn.recv(1024).decode()
-        if not data:
-            break
-        print('[' + time.strftime('%H:%M') + '] ' + user + ": " + str(data))
-        data = (user + ': ' + str(data))
-        #data = str(data).upper()
-        print('[' + time.strftime('%H:%M') + "] sending " + str(data).upper())
-        conn.send(data.encode())
-    print('[' + time.strftime('%H:%M') + '] ' + str(addr) + " disconnected; Closing Thread")
-    conn.close()
+	global index
+	global messages
+	global userCount
+	localIndex = index
+	user = conn.recv(1024).decode()
+	_thread.start_new_thread(sendChat, (conn, localIndex))
+	# print("thread for " + user + " starting loop")
+	while True:
+		data = conn.recv(1024).decode()
+		if not data:
+			break
+		print('[' + time.strftime('%H:%M') + '] ' + user + ": " + str(data))
+		if(data != None):
+			data = (user + ': ' + str(data))
+			addMessage(data)
+			# localIndex += 1
+		# sendData = messages[localIndex]
+		# print('[' + time.strftime('%H:%M') + "] sending " + str(data).upper())
+		# print("sending: '" + sendData + "' to " + user)
+		# conn.send(sendData.encode())
+	conn.close()
+	print('[' + time.strftime('%H:%M') + '] ' + str(addr) + " disconnected; Closing Thread")
+	
 
 def thread():
-    #host = "192.168.1.130"
-    host = "192.168.1.135" #local host for debugging and development
-    port = 7972
-    print("server started on: " + host + ":" + str(port))
-    mySocket = socket.socket()
-    mySocket.bind((host,port))
+	host = "192.168.1.142"
+	# host = "127.0.0.1" #local host for debugging and development
+	port = 7972
+	print("server started on: " + host + ":" + str(port))
+	mySocket = socket.socket()
+	mySocket.bind((host,port))
 
-    while True:
-        mySocket.listen(1)
-        conn, addr = mySocket.accept()
-        _thread.start_new_thread(handling, (conn, addr))
-        print('[' + time.strftime('%H:%M') + "] New thread created for connection from: " + str(addr))
-    mySocket.close()
+	while True:
+		mySocket.listen(1)
+		conn, addr = mySocket.accept()
+		_thread.start_new_thread(handling, (conn, addr))
+		print('[' + time.strftime('%H:%M') + "] New thread created for connection from: " + str(addr))
+	mySocket.close()
 
 def main():
-    thread()
+	thread()
 
 main()
 exit(-1)
