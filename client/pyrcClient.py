e706f38c37ce35ccd57eceb5fc149ff9e3a83922
Levi Schubert <me@levischubert.com>
2018-05-11 12:45:29 -0500

    added user login on server, dynamic user adding, password encryption

--- client/pyrcClient.py
+++ client/pyrcClient.py
@@ -16,93 +16,124 @@ name = ""
 connected = False
 
 def help():
-    print("-------------------------HELP MENU-----------------------------")
-    print("commands:")
-    print("connect | opens the connection menu")
-    print("setName | opens the username settings")
-    print("chat    | attempts to connect to the chat of the current server")
-    print("help    | opens this help page")
-    print("exit    | terminates the program")
+	print("-------------------------HELP MENU-----------------------------")
+	print("commands:")
+	print("connect | opens the connection menu")
+	print("setName | opens the username settings")
+	print("chat	| attempts to connect to the chat of the current server")
+	print("help	| opens this help page")
+	print("exit	| terminates the program")
 
 def connect():
-    global command
-    global connected
-    global host
-    global sock
-    sock = socket.socket()
-    host = "pyrc.tk" #socket.gethostname()
-    connected = True
+	global command
+	global connected
+	global host
+	global sock
+	# answer = ""
+	# if(host != None){
+	# 	print("Would you like to use the previous hostname? (y/n)")
+	# 	answer = input()
+	# }
+	# if(answer == "y"){
+	# 	connected = True
+	# 	print("The connected host is: " + host)	
+	# }else{
+	# 	print("Enter hostname for server to connect to: ")
+	# 	host = input()
+	# 	connected = True
+	# }
+	# sock = socket.socket()
+	connected = True
+	sock = socket.socket()
+	# host = "pyrc.tk"
+	# host = socket.gethostname()
+	host = "127.0.0.1"
 
 def setName():
-    global name
-    name = input("Please choose a username to be displayed on servers: ")
-    print("Your username has been set to: " + name)
+	global name
+	name = input("Please choose a username to be displayed on servers: ")
+	print("Your username has been set to: " + name)
 
 def printChat(sock, dont):
 	while True:
 		data = sock.recv(1024).decode()
 		if not data:
+			chatting = False
 			break
 		print(data)
 
 
 def chat():
-    global connected
-    global name
-    global chatting
-    global sock
-    chatting = True
-    counter = 0
-    if connected == False:
-        print("Error: you are not connected to a server")
-    elif(name == ""):
-        print("Error: you have not set a username")
-    else:
-        print("System: You are now chatting with the server. Use /exit to leave the chat.")
-        sock.connect((host,7972))
-        _thread.start_new_thread(printChat, (sock, None))
-        sock.send(name.encode())
-        while(chatting):
-            arg = input()
-            if(arg.split(' ',1)[0] == "/exit"):
-                chatting = False
-                print("You're now disconnected from the chat")
-            else:
-                print("\033[A                                                                                \033[A")
-                sock.send(arg.encode())
-                # sock.send(("message number: " + str(counter)).encode() )
-                # counter += 1
-    sock.close()
-            #parse input to send chat
+	global connected
+	global name
+	global chatting
+	global sock
+	chatting = True
+	counter = 0
+	if connected == False:
+		print("Error: you are not connected to a server")
+	elif(name == ""):
+		print("Error: you have not set a username")
+	else:
+		print("System: You are now chatting with the server. Use /exit to leave the chat.")
+		sock.connect((host,7972))
+		_thread.start_new_thread(printChat, (sock, None))
+		sock.send(name.encode())
+		while(chatting):
+			arg = input()
+			if(arg.split(' ',1)[0] == "/exit"):
+				chatting = False
+				print("You're now disconnected from the chat")
+			else:
+				print("\033[A																				\033[A")
+				sock.send(arg.encode())
+				# sock.send(("message number: " + str(counter)).encode() )
+				# counter += 1
+	sock.close()
+	connected = False
+			#parse input to send chat
+
+def createUserConfig():
+	NotImplemented
+
+def createUser():
+	NotImplemented
+	createUserConfig()
+
+def onClose():
+	NotImplemented
+
+def login():
+	NotImplemented
 
 def interpret(com):
-    cmd = com.split(' ', 1)[0]
-    if cmd == "connect":
-        connect()
-    elif cmd == "help":
-        help()
-    elif cmd == "setName":
-        setName()
-    elif cmd == "chat":
-        chat()
-    else:
-        print("Error: unrecognized command, type 'help' for a list of commands")
+	cmd = com.split(' ', 1)[0]
+	if cmd == "connect":
+		connect()
+	elif cmd == "help":
+		help()
+	elif cmd == "setName":
+		setName()
+	elif cmd == "chat":
+		chat()
+	else:
+		print("Error: unrecognized command, type 'help' for a list of commands")
 
 
 def main():
-    global command
-    print("Welcome to PyRC [Version 0.1.0]")
-    print("Type 'help' to get started")
-    print("User Test version usage:")
-    print("type 'setName' and follow the prompt to set a username")
-    print("type connect | The ip and port are already set for the test")
-    print("type chat to enter the chat room and connect")
-    command = input()
-    while(command != "exit"):
-        interpret(command)
-        command = input()
-    print("thanks for using PyRC")
-    SystemExit
+	global command
+	print("Welcome to PyRC [Version 0.1.0]")
+	print("Type 'help' to get started")
+	print("User Test version usage:")
+	print("type 'setName' and follow the prompt to set a username")
+	print("type connect | The ip and port are already set for the test")
+	print("type chat to enter the chat room and connect")
+	command = input()
+	while(command != "exit"):
+		interpret(command)
+		command = input()
+	print("thanks for using PyRC")
+	SystemExit
 
 
 main()
--- server/pyrcServer.py
+++ server/pyrcServer.py
@@ -1,17 +1,30 @@
 # PyRC Server [Version 0.1.0]
 # Author: Levi Schubert [me@levischubert.com]
 # Description: a Python Relay Chat server lightweight enough to run on a Raspberry Pi
-#              with AES data encryption
+#			   with AES data encryption
 # Date: 4/25/18
 
 import time
 import _thread
-#import cryptography
+from Crypto.Cipher import AES
 import socket
+import configparser
+import sys
+import getpass
+from io import StringIO
 
 #global variables
 global messages
 global index
+global running
+global logging
+global user
+global loginCipher
+
+#assigning initial global variables
+running = False
+logging = False
+loginCipher = AES.new("default pyrc server login cipher", AES.MODE_CBC, 'default pyrc IV0')
 
 messages = []
 index = 0
@@ -35,8 +48,19 @@ def addMessage(msg):
 	index += 1
 	# print("added '" + msg + "' to global array")
 
+def showLog():
+
+	localIndex = index
+	while logging:
+		if(localIndex != index):
+			print('[' + time.strftime('%H:%M') + '] ' + messages[localIndex])
+			localIndex += 1
+			if(localIndex >= 1000):
+				localIndex = 0
+
+
 def sendChat(conn, localIndex):
-	while True:
+	while running:
 		if(localIndex != index):
 			conn.send(messages[localIndex].encode())
 			localIndex += 1
@@ -51,11 +75,11 @@ def handling(conn, addr):
 	user = conn.recv(1024).decode()
 	_thread.start_new_thread(sendChat, (conn, localIndex))
 	# print("thread for " + user + " starting loop")
-	while True:
+	while running:
 		data = conn.recv(1024).decode()
 		if not data:
 			break
-		print('[' + time.strftime('%H:%M') + '] ' + user + ": " + str(data))
+		# print('[' + time.strftime('%H:%M') + '] ' + user + ": " + str(data))
 		if(data != None):
 			data = (user + ': ' + str(data))
 			addMessage(data)
@@ -69,22 +93,111 @@ def handling(conn, addr):
 	
 
 def thread():
-	host = "192.168.1.142"
-	# host = "127.0.0.1" #local host for debugging and development
+	global running
+	# host = "192.168.1.142"
+	host = "127.0.0.1" #local host for debugging and development
 	port = 7972
-	print("server started on: " + host + ":" + str(port))
+	# print("server started on: " + host + ":" + str(port))
 	mySocket = socket.socket()
+	mySocket.settimeout(5)
 	mySocket.bind((host,port))
-
-	while True:
-		mySocket.listen(1)
-		conn, addr = mySocket.accept()
-		_thread.start_new_thread(handling, (conn, addr))
-		print('[' + time.strftime('%H:%M') + "] New thread created for connection from: " + str(addr))
+	print('[' + time.strftime('%H:%M') + "] Server started on: " + host + ":" + str(port))
+	while running:
+		try:
+			mySocket.listen(1)
+			conn, addr = mySocket.accept()
+			_thread.start_new_thread(handling, (conn, addr))
+			print('[' + time.strftime('%H:%M') + "] New thread created for connection from: " + str(addr))
+		except Exception as err:
+			pass
 	mySocket.close()
+	print('[' + time.strftime('%H:%M') + "] Closing server at: " + host + ":" + str(port))
+
+def newUser():
+	global user
+	user = input("new username: ")
+	config = configparser.ConfigParser()
+	config.read("users.ini")
+	config[user] = {}
+	with open('users.ini', 'w') as configfile:
+			config.write(configfile)
+	setPass()
+	
+
+def setPass():
+	global user
+	global loginCipher
+	password = getpass.getpass(prompt="type  new  password: ", stream=None)
+	confirm = getpass.getpass(prompt="confirm new password: ", stream=None)
+	if(password == confirm):
+		config = configparser.RawConfigParser()
+		config.read("users.ini")
+		config[user] ["password"] = str(loginCipher.encrypt(pad(password)))
+		with open('users.ini', 'w') as configfile:
+			config.write(configfile)
+	else:
+		print("passwords didn't match, password change not applied")
+	NotImplemented
+
+def pad(string):
+	while(not len(string)%16 == 0):
+		string += chr(124)
+	return string
+
+
+def interpret(com):
+	global running
+	global logging
+	if(com == "init" and running != True):
+		running = True
+		_thread.start_new_thread(thread, ())
+	elif(com == "init" and running == True):
+		print("Server already running")
+	elif(com == "close"):
+		running = False
+	elif(com == "toggleLog"):
+		if(logging):
+			logging = False
+		else:
+			logging = True
+			_thread.start_new_thread(showLog, ())
+	elif(com == "newUser"):
+		newUser()
+	elif(com == "setPass"):
+		setPass()
+		
+def login():
+	global user
+	global loginCipher
+	print("Login to PyRC Server")
+	username = input("Username: ")
+	config = configparser.RawConfigParser()
+	config.read("users.ini")
+	if(username in config):
+		password = getpass.getpass(prompt="password: ", stream=None)
+		if((config[username]["password"]) == password):
+			print("successfully logged in")
+			print("[  NOTICE  | YOUR PASSWORD IS STORED IN PLAIN TEXT]")
+			print("[use 'setPass' to change and encrypt your password]")
+			user = username
+			return True
+		elif((config[username]["password"]) == str(loginCipher.encrypt(pad(password)))):
+			print("successfully logged in")
+			user = username
+			return True
+		print((config[username]["password"]) + " vs " + str(loginCipher.encrypt(pad(password))))
+		print("[incorrect password or username, exiting system]")
+	return False
 
 def main():
-	thread()
+	print("|PyRC Server [Version 0.1.0]|")
+	global user
+	if(login()):
+		command = input
+		while(command != "exit"):
+			interpret(command)
+			command = input(user + " ➤ ")
+	
 
 main()
 exit(-1)
